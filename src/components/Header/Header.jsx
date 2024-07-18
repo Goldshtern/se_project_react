d3b74e2b1bf00fc141e1ff4468ec8899ebb96016
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-07-19 02:12:16 +0300

    feat: create Header, begins

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+    </header>
+  );
+}
+
+export default Header;
