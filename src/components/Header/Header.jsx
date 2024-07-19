27f284af4f1fa37792382818860fd2a58e2823a7
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-07-19 16:47:45 +0300

    feat: add header logo and avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
 function Header() {
   return (
     <header className="header">
-      <img className="header__logo" />
+      <img className="header__logo" src={logo} alt="" />
       <p className="header__date-and-location">DATE, LOCATION</p>
       <button className="header__add-clothes-btn">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">NAME</p>
+        <img className="header__avatar" src={avatar} alt="App avatar" />
+      </div>
     </header>
   );
 }
