22d070fe391ee8c0c5b15fe12aff9746b371bb19
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-07-21 15:22:06 +0300

    styling headers components and cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 
 function App() {
-  const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [weatherData, setWeatherData] = useState({ type: "" });
   return (
     <div className="page">
       <div className="page__content">
