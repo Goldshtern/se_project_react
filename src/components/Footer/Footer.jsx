f465c11828bfcef8e62933087b9a8b58ea4675bc
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-07-21 16:50:19 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <ul className="footer__content">
+        <li className="footer__content-name">
+          Designed by Aleksandr Goldshtern
+        </li>
+        <li className="footer__content-year"> 2024</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
