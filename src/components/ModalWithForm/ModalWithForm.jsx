99e5cdac97a92d4cd801363507c8031182475be6
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-07-24 22:00:52 +0300

    feat: add Item Moal popup open and close

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,23 +1,13 @@
 import "./ModalWithForm.css";
 import closeBtn from "../../assets/cls-btn.svg";
-function ModalWithForm({
-  children,
-  buttonText,
-  title,
-  activeModal,
-  handleCloseClick,
-}) {
+function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div
       className={`modal ${activeModal === "add-garment" && "modal__opened"}`}
     >
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button
-          onClick={handleCloseClick}
-          type="button"
-          className="modal__close"
-        >
+        <button onClick={onClose} type="button" className="modal__close">
           <img src={closeBtn} alt="close button" className="modal__close-btn" />
         </button>
         <form className="modal__form">
