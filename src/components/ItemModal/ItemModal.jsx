c34bac12ef6262e74ddc3d8c944071f47cf98540
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-08-18 12:56:31 +0300

    style: ItemCard.jsx component; feat: add delete card button in ItemModal.jsx component

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -12,6 +12,9 @@ function ItemModal({ activeModal, onClose, card }) {
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
+          <button className="modal__delete-btn" onClick={onClose}>
+            Delete Item
+          </button>
         </div>
       </div>
     </div>
