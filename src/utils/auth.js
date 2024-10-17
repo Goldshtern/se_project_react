f127481911557dbfb9cd65bb42805bd21889626d
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-10-17 12:32:53 +0300

    fix: changes routes on /signup and /signin in auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,18 +3,18 @@ import { checkResponse } from "./api";
 const baseUrl = "http://localhost:3001";
 const headers = { "Content-Type": "application/json" };
 
-function signUp() {
-  return fetch(`${baseUrl}/profile`, {
+function signUp(name, avatarUrl, email, password) {
+  return fetch(`${baseUrl}/signup`, {
     method: "POST",
     headers,
-    body: JSON.stringify({ name, avatar, email, password }),
+    body: JSON.stringify({ name, avatarUrl, email, password }),
   }).then(checkResponse);
 }
 
 export { signUp };
 
 function signIn() {
-  return fetch(`${baseUrl}/profile`, {
+  return fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers,
     body: JSON.stringify({ email, password }),
