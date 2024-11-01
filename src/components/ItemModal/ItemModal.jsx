0fa14837ec9781fe770d29a987a4d94bc0f05925
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-11-02 00:55:13 +0200

    style: add style at ItemCard.css , ItemModal.css

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -29,13 +29,10 @@ function ItemModal({ activeModal, onClose, card, handleDeleteItem }) {
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
           <button
-            //className="modal__delete-btn"
-            //onClick={() => handleDeleteItem(card) && onClose}
             type="button"
             className={itemDeleteButtonClassName}
             onClick={handleDeleteClick}
           >
-            {" "}
             Delete item
           </button>
         </div>
